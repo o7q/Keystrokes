dc752570c42bb66c2d3ddd26f22eb1780c2221ad
o7q <jev.o7q@gmail.com>
2023-05-07 23:05:56 -0500

    Implement the download website

---
+++ assets/pages/index/files/scripts.js
@@ -0,0 +1,12 @@
+function bodyInit()
+{
+    dlConfig();
+}
+
+function dlConfig()
+{
+    var ver = "v1.5.1.0";
+
+    document.getElementById("dlID").href = "https://github.com/o7q/Keystrokes/releases/download/" + ver + "/Keystrokes.exe";
+    document.getElementById("dlID").innerHTML = "Download " + ver;
+}
