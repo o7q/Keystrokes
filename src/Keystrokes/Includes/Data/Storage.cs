1b1a4b044f2f082d2f3e5d2784f415135328e488
o7q <jev.o7q@gmail.com>
2023-05-14 21:08:15 -0500

    Forgot to update version text

--- src/Keystrokes/Includes/Data/Storage.cs
+++ src/Keystrokes/Includes/Data/Storage.cs
@@ -5,7 +5,7 @@ namespace Keystrokes.Data
 {
     public static class Storage
     {
-        public static string VERSION = "v1.6.0";
+        public static string VERSION = "v1.6.1";
         public static List<Form> keys = new List<Form>();
     }
 }
